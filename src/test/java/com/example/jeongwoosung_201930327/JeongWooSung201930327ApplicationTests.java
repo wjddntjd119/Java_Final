c1324176776458631fdde8911501d28b52dd150d
정우성 <helloWorld@java.com>
2023-06-08 14:39:43 +0900

    init

---
+++ src/test/java/com/example/jeongwoosung_201930327/JeongWooSung201930327ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jeongwoosung_201930327;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeongWooSung201930327ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
